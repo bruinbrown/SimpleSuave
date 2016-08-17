bb2f514bf2b6c26c218562d92a185557eb0e393d
Anthony Brown <bruinbrown@gmail.com>
2016-08-17 20:17:48 +0100

    Initial commit

---
+++ SimpleSuave/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open System.Net
+
+[<EntryPoint>]
+let main [| port |] = 
+    
+    let config = { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Loopback (uint16 port) ] }
+
+
+    let app =
+        choose
+            [ GET >=> path "/hello" >=> OK "Hello GET"
+              pathScan "/hello/%s" (fun name -> OK("Hello " + name)) ]
+
+
+    startWebServer config app
+
+    0
+
+    
